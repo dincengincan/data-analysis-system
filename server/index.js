9e3b5a22b033fa244f792db5b350b2eefe24716f
Engin Can Dinç <engincandinc@gmail.com>
2023-12-25 16:52:47 +0300

    Improve naming in some files

--- server/index.js
+++ server/index.js
@@ -3,9 +3,7 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 
 import connectDB from "./mongodb/connect.js";
-import geneRouter from "./routes/genes.route.js";
-import Gene from "./mongodb/models/gene.js";
-import { mockData } from "./data/mock.js";
+import genesRouter from "./routes/genes.route.js";
 
 dotenv.config();
 
@@ -14,11 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.use("/api/genes", geneRouter);
+app.use("/api/genes", genesRouter);
 
 const startServer = async () => {
   try {
@@ -27,9 +21,6 @@ const startServer = async () => {
     app.listen(8080, () => {
       console.log("Server started on port http://localhost:8080");
     });
-
-    // ADD MOCK DATA HERE
-    // Gene.insertMany(mockData);
   } catch (error) {
     console.log(error);
   }
